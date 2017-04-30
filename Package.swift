7a7f3dfbc7bf264e99db2c8f7f7f8877054c4066
Stanislav Safronov <killannn@gmail.com>
2017-04-30 10:44:13 +0300

    fixed package

--- Package.swift
+++ Package.swift
@@ -4,8 +4,6 @@ let package = Package(
     name: "Objc",
     targets: [Target(name: "cpp-exec", dependencies:["cpplib"]),
               Target(name: "cwrapper", dependencies:["cpplib"]),
-              Target(name: "swift-exec", dependencies:["objcwrapper"]),
-              Target(name: "objcwrapper", dependencies:["cwrapper"])
-        
+              Target(name: "swift-exec", dependencies:["cwrapper"])
     ]
 )
